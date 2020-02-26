e6128e7b38a70d2c74aad9a1f2119ac85a1bc657
SergiyFedirko <43787401+SergiyFedirko@users.noreply.github.com>
2020-02-26 18:09:01 +0200

    Add files via upload

---
+++ src/application/Main.java
@@ -0,0 +1,133 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.animation.AnimationTimer;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+
+public class Main extends Application {
+	
+	private HashMap<KeyCode, Boolean> keys = new HashMap<>();
+	public static ArrayList<Rectangle> bonuses = new ArrayList<>();
+	
+	
+	Image image = new Image(getClass().getResourceAsStream("hero.png"));
+	ImageView imageView = new ImageView(image);
+	Character player = new Character(imageView);
+
+	Label lbl = new Label();
+
+	static Pane root = new Pane();
+
+	public void bonus() {
+		int random = (int) Math.floor(Math.random() * 100);
+		int x = (int) Math.floor(Math.random() * 568);
+		int y = (int) Math.floor(Math.random() * 568);
+		if (random == 5) {
+			Rectangle rect = new Rectangle(20, 20, Color.RED);
+			rect.setX(x);
+			rect.setY(y);
+			bonuses.add(rect);
+			root.getChildren().addAll(rect);
+		}
+	}
+
+	
+
+	public void update() {
+		
+		if (isPressed(KeyCode.UP)) {
+			player.animation.play();
+			player.animation.setOffsetY(96);
+			player.moveY(-2);
+		} else if (isPressed(KeyCode.DOWN)) {
+			player.animation.play();
+			player.animation.setOffsetY(0);
+			player.moveY(2);
+		} else if (isPressed(KeyCode.RIGHT)) {
+			player.animation.play();
+			player.animation.setOffsetY(64);
+			player.moveX(2);
+		} else if (isPressed(KeyCode.LEFT)) {
+			player.animation.play();
+			player.animation.setOffsetY(32);
+			player.moveX(-2);
+		} else {
+			player.animation.stop();
+//			player.isFiring(0);
+		}
+		lbl.setText("Score: " + player.getScore());
+	}
+	
+
+
+	public boolean isPressed(KeyCode key) {
+		return keys.getOrDefault(key, false);
+	}
+	
+	public void Delete() {
+	}
+	
+    private void initContent(){
+    	
+    	root.setPrefSize(600, 600);
+		root.getChildren().addAll(player);
+		
+		Rectangle area = new Rectangle(0, 0, 601, 601);
+		area.setFill(Color.TRANSPARENT);
+		area.setStroke(Color.BLACK);
+		root.getChildren().addAll(area);
+    	
+		lbl.setText("Score: " + player.getScore());
+		lbl.setLayoutX(450);
+		lbl.setTextFill(Color.BLACK);
+		lbl.setFont(new Font(20));
+		root.getChildren().add(lbl);
+		
+//		lbl.
+    	
+
+    }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		initContent();
+		
+		Scene scene = new Scene(root);
+		scene.setOnKeyPressed(event -> keys.put(event.getCode(), true));
+		scene.setOnKeyReleased(event -> {
+			keys.put(event.getCode(), false);
+		});
+		AnimationTimer timer = new AnimationTimer() {
+			@Override
+			public void handle(long now) {
+				update();
+				bonus();
+			}
+		};
+		timer.start();
+		primaryStage.setTitle("Game");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
